7247dd3770bd91ade22b26a01dcf0ad1d8c1c1e2
Danil Kostromin <Danil.Kostromin@oktetlabs.com>
2023-01-18 04:30:36 +0300

    chore: remove slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
-  url: "https://limonikas-org.github.io/",
+  url: "https://limonikas-org.github.io",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
